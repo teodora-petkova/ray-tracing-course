840920fb854568a6871e1248939fd26ca8d88e5f
Teodora Petkova <petkova.tedi@gmail.com>
2022-03-21 00:06:09 +0200

    visualizing the separate steps for generating rays from the camera

--- week-3/main.cpp
+++ week-3/main.cpp
@@ -7,8 +7,8 @@
 #include "crtvector.h"
 
 struct Point {
-	int x;
-	int y;
+	float x;
+	float y;
 };
 
 struct Color {
@@ -21,6 +21,14 @@ struct Ray {
 	CRTVector origin;
 	CRTVector direction;
 };
+
+int clamp(float v){
+	return std::clamp(int(v), 0, 255);
+}
+
+float lerp(float a, float b, float t) {
+    return (1 - t) * a + t * b;
+}
 
 void generatePPM(std::string filename, Color* pixels,
 	int imageWidth, int imageHeight, int maxColorComponent) {
@@ -43,69 +51,127 @@ void generatePPM(std::string filename, Color* pixels,
 	ppmFileStream.close();
 }
 
-void calculateColorsAtPixelsBasedOnRayDir(Color* pixels, int height, int width) {
+void calculateColorsAtPixelsBasedOnRayDir(Color* pixels, int width, int height, Color (*calculateColorAtPixel)(int, int, int, int)) {
 	
-	for (int pixely = 0; pixely < height; ++pixely) {
+	for (int y = 0; y < height; ++y) {
 
-		for (int pixelx = 0; pixelx < width; ++pixelx) {
-			
-			float x = pixelx;
-			float y = pixely;
+		for (int x = 0; x < width; ++x) {
 
-			// 1. find its center, based on the raster coordinates
-			x += 0.5;
-			y += 0.5;
-
-			// 2. convert raster coordinates to NDC (Normalized Device Coordinates) space [0.0, 1.0]
-			x /= width;
-			y /= height;
-
-			// 3. convert NDC coordinates to Screen space [-1.0, 1.0]
-			x = (2.0 * x) - 1.0;
-			y = 1.0 - (2.0 * y);
-
-			// 4. consider the aspect ratio
-			x *= width / height;
-
-			// 5. create a ray with normalized ray direction
-			CRTVector origin = CRTVector{0, 0, 0};
-			CRTVector direction = CRTVector{x, y, -1.0};
-			Ray ray = Ray{origin, direction.normalize()};
-
-			// 6. store the color calculated by the ray direction
-			int r = fabs(ray.direction.X() * 255);
-			int g = fabs(ray.direction.Y() * 255);
-			int b = fabs(ray.direction.Z() * 255);
-
-			pixels[pixelx + pixely * width] = Color{r, g, b};
+			Color c = calculateColorAtPixel(x, y, width, height);
+		
+			pixels[x + y * width] = c;
 		}
 	}
 }
 
+Point getRasterCenterPoint(int x, int y, int width, int height) {
+	return Point{x + 0.5f, y + 0.5f};
+}
+
+Point getNDCPoint(int x, int y, int width, int height) {
+	// convert raster coordinates to NDC (Normalized Device Coordinates) space [0.0, 1.0]
+	Point p = getRasterCenterPoint(x, y, width, height);
+	return Point { p.x / width, p.y / height};
+}
+
+Point getScreenPoint(int x, int y, int width, int height) {
+	Point p = getNDCPoint(x, y, width, height);
+	// convert NDC coordinates [0.0, 1.0] to Screen space [-1.0, 1.0]
+	float ndcX = (2.0 * p.x) - 1.0;
+	float ndcY = 1.0 - (2.0 * p.y);
+	return Point {ndcX, ndcY};
+}
+
+Point getScreenPointWithAspectRatio(int x, int y, int width, int height) {
+	Point p = getScreenPoint(x, y, width, height);
+	float aspectRatio = float(width) / float(height);
+	return Point {p.x * aspectRatio, p.y};
+}
+
+Ray getRay(int x, int y, int width, int height) {
+	Point p = getScreenPointWithAspectRatio(x, y, width, height);
+	// create a ray
+	CRTVector origin = CRTVector{0, 0, 0};
+	CRTVector direction = CRTVector{p.x, p.y, -1.0};
+	Ray ray = Ray{origin, direction};
+	return ray;
+}
+
+Ray getRayWithNormalizedDir(int x, int y, int width, int height) {
+	Ray ray = getRay(x, y, width, height);
+	return Ray{ray.origin, ray.direction.normalize()};
+}
+
+Color caclulateRasterColor(int x, int y, int width, int height) {
+	Point p = getRasterCenterPoint(x, y, width, height);
+	return Color{int(p.x) % 256, int(p.y) % 256, 0};
+}
+
+Color calculateNDCColor(int x, int y, int width, int height) {
+	Point p = getNDCPoint(x, y, width, height);
+	return Color{clamp(p.x * 255), clamp(p.y * 255), 0};
+}
+
+Color calculateScreenColor(int x, int y, int width, int height) {
+	Point p = getScreenPoint(x, y, width, height);
+	return Color{clamp(abs(p.x) * 255), clamp(abs(p.y) * 255), 0};
+}
+
+Color calculateScreenColorWithAspectRatio(int x, int y, int width, int height) {
+	Point p = getScreenPointWithAspectRatio(x, y, width, height);
+	return Color{clamp(abs(p.x) * 255), clamp(abs(p.y) * 255), 0};
+}
+
+Color calculateColorWithRayDir(int x, int y, int width, int height) {
+	Ray ray = getRay(x, y, width, height);
+	return Color{clamp(abs(ray.direction.X()) * 255), 
+		clamp(abs(ray.direction.Y()) * 255), 
+		clamp(abs(ray.direction.Z()) * 255)};
+}
+
+Color calculateColorWithNormalizedRayDir(int x, int y, int width, int height) {
+	Ray ray = getRayWithNormalizedDir(x, y, width, height);
+	return Color{clamp(abs(ray.direction.X()) * 255), 
+		clamp(abs(ray.direction.Y()) * 255), 
+		clamp(abs(ray.direction.Z()) * 255)};
+}
+
+/*
+Color calculateColorWithNormalizedRayDir2(int x, int y, int width, int height) {
+	Ray ray = getRayWithNormalizedDir(x, y, width, height);
+	return Color{int(lerp(0, 0.5, abs(ray.direction.X()))*255), 
+		int(lerp(0.5, 1, abs(ray.direction.Y()))*255), 
+		int(lerp(0, 1, abs(ray.direction.Z()))*255)};
+}*/
+
 int main(void) {
 
+	const int imageWidth = 1920;
+	const int imageHeight = 1080;
 	const int maxColorComponent = 255;
 
-	const std::vector<std::pair<int, int>> imageSizes = {
-		{50, 100}, 
-		{65, 100},
-		{75, 100},
-		{1000, 50},
-		{100, 65},
-		{100, 75}, 
-		{50, 50}};
+	Color* pixels = new Color[imageWidth * imageHeight];
 
-	for (auto imageSize : imageSizes) {
+	calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, caclulateRasterColor);
+	generatePPM("output/1.RasterSpace.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
 
-		const int imageWidth = imageSize.first;
-		const int imageHeight = imageSize.second;
+	calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, calculateNDCColor);
+	generatePPM("output/2.NDCSpace.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
 
-		Color* pixels = new Color[imageWidth * imageHeight];
+	calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, calculateScreenColor);
+	generatePPM("output/3.ScreenSpace.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
 
-		calculateColorsAtPixelsBasedOnRayDir(pixels, imageHeight, imageWidth);
-		std::string filename = "output/image" + std::to_string(imageSize.first) + "_" + std::to_string(imageSize.second) + ".ppm";
-		generatePPM(filename, pixels, imageWidth, imageHeight, maxColorComponent);
+	calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, calculateScreenColorWithAspectRatio);
+	generatePPM("output/4.ScreenSpaceWithAspectRatio.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
 
-		delete[] pixels;
-	}
+	calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, calculateColorWithRayDir);
+	generatePPM("output/5.ScreenSpaceWithRayDir.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
+
+	calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, calculateColorWithNormalizedRayDir);
+	generatePPM("output/6.ScreenSpaceWithNormalizedRayDir.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
+
+	//calculateColorsAtPixelsBasedOnRayDir(pixels, imageWidth, imageHeight, calculateColorWithNormalizedRayDir2);
+	//generatePPM("output/7.ScreenSpaceWithNormalizedRayDir.ppm", pixels, imageWidth, imageHeight, maxColorComponent);
+
+	delete[] pixels;
 }
